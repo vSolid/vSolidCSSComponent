52f5aab7bb16808477304946ecd73582d7648e87
Sebastian Livoni <29739749+sebastianlivoni@users.noreply.github.com>
2024-04-26 09:03:31 +0200

    nice

--- src/ArchiveGetOperationHandler.ts
+++ src/ArchiveGetOperationHandler.ts
@@ -1,32 +1,31 @@
-import { BasicRepresentation, ETagHandler, FileDataAccessor, GetOperationHandler, HttpRequest, INTERNAL_QUADS, OkResponseDescription, Operation, OperationHandler, OperationHttpHandlerInput, RepresentationMetadata, ResourceIdentifier, ResourceStore, ResponseDescription, endOfStream, readableToQuads, readableToString } from "@solid/community-server"
+import { BasicRepresentation, ETagHandler, FileDataAccessor, GetOperationHandler, Guarded, HttpRequest, INTERNAL_QUADS, OkResponseDescription, Operation, OperationHandler, OperationHttpHandlerInput, Representation, RepresentationMetadata, ResourceIdentifier, ResourceStore, ResponseDescription, endOfStream, guardStream, readableToQuads, readableToString } from "@solid/community-server"
 import { QueryEngine } from '@comunica/query-sparql'
-import { Readable } from "stream"
-import { Store, Parser, Quad } from "n3"
+import { Duplex, Readable } from "stream"
+import { Store, Parser, StreamWriter } from "n3"
 import { Bindings, BindingsStream } from "@comunica/types"
 import arrayifyStream from 'arrayify-stream'
 import { RdfDatasetRepresentation } from "@solid/community-server/dist/http/representation/RdfDatasetRepresentation"
 import { inspect, promisify } from "node:util"
 
 export class ArchiveGetOperationHandler extends GetOperationHandler {
-    private readonly _store: ResourceStore;
+    private readonly _store: ResourceStore
     private readonly engine
 
     public constructor(store: ResourceStore, eTagHandler: ETagHandler) {
-        super(store, eTagHandler);
-        this._store = store;
+        super(store, eTagHandler)
+        this._store = store
         this.engine = new QueryEngine()
     }
 
     public async handle({ request, operation }: OperationHttpHandlerInput): Promise<ResponseDescription> {
-        let query = this.getSparqlQuery(request)
-        if (!query) {
-            return await super.handle({ operation })
+        if (request.headers["content-type"] == "application/sparql-archiving") {
+            return await this.handleQuery(operation)
         } else {
-            return await this.handleQuery(operation, query)
+            return await super.handle({ operation })
         }
     }
 
-    public async handleQuery(operation: Operation, query: String): Promise<ResponseDescription> {
+    public async handleQuery(operation: Operation): Promise<ResponseDescription> {
         let identifier = operation.target
         let delta_identifier = this.getDeltaIdentifier(identifier)
 
@@ -36,44 +35,65 @@ export class ArchiveGetOperationHandler extends GetOperationHandler {
             representation as RdfDatasetRepresentation :
             new BasicRepresentation() as RdfDatasetRepresentation
 
-
         let str = await this.readStream(representation.data)
-        const parser = new Parser();
-        let existingQuads = parser.parse(str);
+        const parser = new Parser()
+        let existingQuads = parser.parse(str)
         let store = new Store()
         store.addQuads(existingQuads)
 
         inputRepresentation.dataset = store
 
-        const bindingsStream = await this.engine.queryBindings(`${query}`, { sources: [store] })
-        const bindings: Bindings[] = await arrayifyStream(bindingsStream)
+        const sparql = await readableToString(operation.body.data);
+        const quadStream = await this.engine.queryQuads(sparql, { sources: [store], baseIRI: delta_identifier.path })
+        const quads = await quadStream.toArray()
 
-        bindings.forEach((binding) => {
-            console.log(binding.toString())
+        return new OkResponseDescription(new RepresentationMetadata(), this.generateStreamFromArray(quads))
+    }
+
+    private generateStreamFromArray<T>(values: T[]): Guarded<Readable> {
+        const writer = new StreamWriter({ format: 'Turtle' })
+        const ttl: string[] = []
+
+        const duplexStream = new Duplex({
+            read(size) {
+                // No need to implement if only writing is required
+            },
+            write(chunk, encoding, callback) {
+                const str = chunk.toString()
+                if (str) {
+                    ttl.push(str)
+                }
+                callback()
+            },
+            final(callback) {
+                callback()
+            }
         })
 
-        let representation2 = await this._store.getRepresentation(delta_identifier, operation.preferences, operation.conditions)
-
-        return new OkResponseDescription(representation2.metadata, representation2.data)
+        writer.pipe(duplexStream)
+        values.forEach(q => writer.write(q))
+        writer._flush((err) => { if (err) { console.error(err) } })
+        writer.end()
+        const readableStream = Readable.from(ttl)
+        return guardStream(readableStream)
     }
 
     private async readStream(stream: Readable): Promise<string> {
         return new Promise<string>((resolve, reject) => {
-            let data = '';
+            let data = ''
 
             stream.on('data', (chunk: any) => {
-                data += chunk?.toString() ?? '';
-            });
-
+                data += chunk?.toString() ?? ''
+            })
 
             stream.on('end', () => {
-                resolve(data);
-            });
+                resolve(data)
+            })
 
             stream.on('error', (err: Error) => {
-                reject(err);
-            });
-        });
+                reject(err)
+            })
+        })
     }
 
     private getDeltaIdentifier(fromIdentifier: ResourceIdentifier): ResourceIdentifier {
