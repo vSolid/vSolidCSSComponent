73b2e50a49ee1e9b09c9e8964f418d6e7d693aa3
Sebastian Livoni <29739749+sebastianlivoni@users.noreply.github.com>
2024-04-30 09:59:52 +0200

    changed content type version query

--- src/utils/ContentTypes.ts
+++ src/utils/ContentTypes.ts
@@ -1,2 +1,2 @@
-export const APPLICATION_SPARQL_VERSION_QUERY = "application/sparql-version-query";
+export const APPLICATION_SPARQL_VERSION_QUERY = "application/version-query";
 export const APPLICATION_SPARQL_VERSION_MATERIALIZATION = "application/version-materialization";
