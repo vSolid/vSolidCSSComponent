341a1d81a808162988f2561dfa3a8214252d6a8f
Lasse Fjellanger <lassefj0@gmail.com>
2024-05-06 12:42:07 +0200

    fixed k6benchmark.sh

--- k6benchmark.sh
+++ k6benchmark.sh
@@ -18,22 +18,6 @@ run_benchmark() {
         return
     fi
 
-    # Start the node process with output redirection to server.log
-    echo "Starting node server..."
-    npm run startNoBuild > server.log 2>&1 &
-    nodePID=$!
-    sleep 2  # give some time to start up
-
-    # Wait for server to be ready by monitoring server.log
-    echo "Checking for server readiness..."
-    while ! grep -q "Listening to server at http://localhost:3000/" server.log; do
-        echo "Waiting for server to start..."
-        sleep 1
-        # Ensure latest data is being read
-        tail -n 10 server.log > /tmp/latest_server_log
-    done
-    echo "SERVER READY"
-
     # Construct the command for running the benchmark
     fullPath=$(realpath "$benchmarkRoot/$benchmarkName")
     command="k6 run $fullPath --out csv=bencherResults.csv"
@@ -43,13 +27,33 @@ run_benchmark() {
     $command
     echo "Benchmark completed."
 
-    # Kill the node process
-    kill $nodePID
-    wait $nodePID
-    echo "Benchmark completed and node server stopped."
 }
+
+# Start the node process with output redirection to server.log
+echo "Starting node server..."
+npm run startNoBuild > server.log 2>&1 &
+nodePID=$!
+
+echo "Node server started with PID: $nodePID"    
+sleep 2  # give some time to start up
+
+
+# Wait for server to be ready by monitoring server.log
+echo "Checking for server readiness..."
+while ! grep -q "Listening to server at http://localhost:3000/" server.log; do
+    echo "Waiting for server to start..."
+    sleep 1
+    # Ensure latest data is being read
+    tail -n 10 server.log > /tmp/latest_server_log
+done
+echo "SERVER READY"
 
 # Loop over benchmarks and run them
 for bench in "${!benchmarks[@]}"; do
     run_benchmark "$bench"
 done
+
+PORT_NUMBER=3000
+lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill 
+echo "Benchmark completed and node server stopped."
+
